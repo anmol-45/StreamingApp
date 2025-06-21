96a3f5d708a0aee76921153118da5d6df3675d8f
Anmol Kumar <anmolkumar8862@gmail.com>
2025-06-21 23:38:22 +0530

    Initialized microservices app with frontend and backend structure

---
+++ backend/video-stream-service/src/test/java/com/stream/app/video_stream_service/VideoStreamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.app.video_stream_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/video-upload-service/src/main/java/com/stream/app/video_upload_service/VideoUploadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stream.app.video_upload_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoUploadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoUploadServiceApplication.class, args);
+	}
+
+}
